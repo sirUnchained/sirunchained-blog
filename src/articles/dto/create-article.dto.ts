564b73e7d9f0a35f83c7316ea805de6c143534b9
sirUnchained <bbehnam3831@gmail.com>
2025-01-15 12:52:58 +0330

    finished articles entity

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,19 @@
-export class CreateArticleDto {}
+import { IsNotEmpty, IsNumber, Length, Min } from 'class-validator';
+
+export class CreateArticleDto {
+  @IsNotEmpty()
+  @Length(8, 100)
+  title: string;
+
+  @IsNotEmpty()
+  @Length(8, 250)
+  description: string;
+
+  @IsNotEmpty()
+  content: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  category: number;
+}
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,45 @@
-export class Article {}
+import { CategoryEntity } from 'src/categories/entities/category.entity';
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('articles')
+export class ArticleEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: false, unique: true })
+  slug: string;
+
+  @Column({ type: 'string', length: 250 })
+  description: string;
+
+  @Column({ type: 'string', nullable: true })
+  cover: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @OneToMany(() => UserEntity, (user) => user.id, { onDelete: 'CASCADE' })
+  author: UserEntity;
+
+  @OneToMany(() => CategoryEntity, (category) => category.id, {
+    onDelete: 'CASCADE',
+  })
+  category: CategoryEntity;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
