3d2f1ae328f0425d8e1f94c72a26fe50af5aa730
sirUnchained <bbehnam3831@gmail.com>
2025-01-18 10:34:40 +0330

    seems like comment entity finished

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateCommentDto {
+  @Length(5, 1000)
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,30 @@
-export class Comment {}
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('comments')
+export class CommentEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 1000 })
+  content: string;
+
+  @ManyToOne(() => UserEntity, (user) => user.id, { onDelete: 'CASCADE' })
+  user: UserEntity;
+
+  @Column()
+  replies: CommentEntity[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
